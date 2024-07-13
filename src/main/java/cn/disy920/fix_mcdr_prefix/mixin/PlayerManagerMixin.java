d99ba734b61710d63276a5f0a37ebc321d8ebbf6
Liyan Zhao <return65535@qq.com>
2024-07-13 22:30:25 +0800

    fix

--- src/main/java/cn/disy920/fix_mcdr_prefix/mixin/PlayerManagerMixin.java
+++ src/main/java/cn/disy920/fix_mcdr_prefix/mixin/PlayerManagerMixin.java
@@ -1,5 +1,6 @@
 package cn.disy920.fix_mcdr_prefix.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.MinecraftServer;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -19,39 +21,17 @@ import java.util.function.Predicate;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
-
-    @Unique
-    private final Map<String, ServerPlayerEntity> playerCache = new ConcurrentHashMap<>(16);
-
-    @Inject(
+    @ModifyArg(
             method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Ljava/util/function/Predicate;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/network/message/MessageType$Parameters;)V",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/server/MinecraftServer;logChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageType$Parameters;Ljava/lang/String;)V"
             )
     )
-    private void injectBroadcast(SignedMessage message, Predicate<ServerPlayerEntity> shouldSendFiltered, ServerPlayerEntity sender, MessageType.Parameters params, CallbackInfo ci) {
-        if (message.getContent().getString().startsWith("!!")) {
-            playerCache.put(Thread.currentThread().getName(), sender);
+    private MessageType.Parameters redirectedServerLog(MessageType.Parameters params, @Local(argsOnly = true) ServerPlayerEntity sender) {
+        if (params.type() == MessageType.CHAT) {
+            return MessageType.params(MessageType.CHAT, sender.getRegistryManager(), sender.getName());
         }
-    }
-
-    @Redirect(
-            method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Ljava/util/function/Predicate;Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/network/message/MessageType$Parameters;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/server/MinecraftServer;logChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageType$Parameters;Ljava/lang/String;)V"
-            )
-    )
-    private void redirectedServerLog(MinecraftServer server, Text message, MessageType.Parameters params, String prefix) {
-        if (message.getString().startsWith("!!")) {
-            ServerPlayerEntity player = playerCache.get(Thread.currentThread().getName());
-            if (player != null) {
-                params = MessageType.params(MessageType.CHAT, player.getWorld().getRegistryManager(), player.getName());
-            }
-            playerCache.remove(Thread.currentThread().getName());
-        }
-
-        server.logChatMessage(message, params, prefix);
+        return params;
     }
 }
